e0880a6f6dc39cb4b3c6d5373cee5609b0d34db6
Rachel Sherard <rsherard@mac.com>
2024-01-21 01:58:42 -0500

    set up navigation on all pages

--- sanity.config.ts
+++ sanity.config.ts
@@ -22,6 +22,12 @@ import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schema";
 
+// Define the actions that should be available for singleton documents
+const singletonActions = new Set(["publish", "discardChanges", "restore"]);
+
+// Define the singleton document types
+const singletonTypes = new Set(["settings"]);
+
 import { ImagesIcon } from "@sanity/icons";
 import { BinaryDocumentIcon } from "@sanity/icons";
 import { CommentIcon } from "@sanity/icons";
@@ -63,11 +69,9 @@ export default defineConfig({
 							),
 						...S.documentTypeListItems().filter(
 							(listItem) =>
-								![
-									"slideshow",
-									"about",
-									"shoutouts",
-								].includes(listItem.getId() ?? "default")
+								!["slideshow", "about", "shoutouts"].includes(
+									listItem.getId() ?? "default"
+								)
 						),
 					]),
 		}),
@@ -75,5 +79,22 @@ export default defineConfig({
 	],
 	schema: {
 		types: schemaTypes,
+
+		// Filter out singleton types from the global “New document” menu options
+		templates: (templates) =>
+			templates.filter(
+				({ schemaType }) => !singletonTypes.has(schemaType)
+			),
+	},
+
+	document: {
+		// For singleton types, filter out actions that are not explicitly included
+		// in the `singletonActions` list defined above
+		actions: (input, context) =>
+			singletonTypes.has(context.schemaType)
+				? input.filter(
+						({ action }) => action && singletonActions.has(action)
+				  )
+				: input,
 	},
 });
